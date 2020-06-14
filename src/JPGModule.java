2c0c40a89906043d702b8afba70b9cb6ce955fcc
limjustin <limjyjustin@naver.com>
2020-06-14 20:20:56 +0900

    20200614

--- src/JPGModule.java
+++ src/JPGModule.java
@@ -1,12 +1,15 @@
 public class JPGModule implements ImageViewerInterface {
+    @Override
     public String getName() {
         return "JPG Module";
     }
 
+    @Override
     public void show(String fileName) {
         System.out.println("JPG image loading: " + fileName);
     }
 
+    @Override
     public String getExtension() {
         return "JPG";
     }
--- src/PNGModule.java
+++ src/PNGModule.java
@@ -1,12 +1,15 @@
 public class PNGModule implements ImageViewerInterface {
+    @Override
     public String getName() {
         return "PNG Module";
     }
 
+    @Override
     public void show(String fileName) {
         System.out.println("PNG image loading: " + fileName);
     }
 
+    @Override
     public String getExtension() {
         return "PNG";
     }
